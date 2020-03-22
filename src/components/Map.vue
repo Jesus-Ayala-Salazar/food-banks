f3573c5d182582c868501152e2d99c82cf026568
jaayala <jaayala@uci.edu>
2020-03-22 15:38:44 -0700

    markers can now be drawn to map from JSON file

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,27 +5,49 @@
 <script>
   import mapboxgl from 'mapbox-gl';
   import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-  // or "const mapboxgl = require('mapbox-gl');"
-    export default {
-      name: 'Map',
-      data() {return {}},
-      mounted() {
-        this.createMap();
+
+  export default {
+    name: 'Map',
+    data() {return {}},
+    mounted() {
+      this.createMap();
+    },
+    methods: {
+      /* initializes Map bounded to US */
+      createMap: function () {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiamFheWFsYSIsImEiOiJjazgxMnIxbTgwNmtsM2xwNG85MXNybTZvIn0.jgVIhoWGvlCIU5TQoEgnvA';
+        this.map = new mapboxgl.Map({
+          container: 'map',
+          bounds: [[-128.8, 23.6], [-65.4, 50.2]],
+          style: 'mapbox://styles/mapbox/dark-v10'
+        });
+        /* initializes Geocoder Search box*/
         this.map.addControl(
           new MapboxGeocoder({
             accessToken: mapboxgl.accessToken,
+            countries: 'us',
             mapboxgl: mapboxgl
-          }))
+          }));
+        /* adds geojson sources to map */
+        this.map.on('load', this.addLayer);
       },
-      methods: {
-        createMap: function () {
-          mapboxgl.accessToken = 'pk.eyJ1IjoiamFheWFsYSIsImEiOiJjazgxMnIxbTgwNmtsM2xwNG85MXNybTZvIn0.jgVIhoWGvlCIU5TQoEgnvA';
-          this.map = new mapboxgl.Map({
-            container: 'map',
-            bounds: [[-128.8, 23.6], [-65.4, 50.2]],
-            style: 'mapbox://styles/mapbox/light-v10'
-          });
-        }
+      addLayer: function () {
+        this.map.addLayer({
+          id: 'main-layer',
+          paint: {
+            'circle-opacity': 0.5,
+            'circle-radius': 7,
+            'circle-stroke-color': '#fff',
+            'circle-stroke-width': 1,
+            'circle-color': '#f683fc'
+          },
+          source: {
+            data: require('../assets/sample'),
+            type: 'geojson',
+          },
+          type: 'circle',
+        });
       }
     }
+  }
 </script>
