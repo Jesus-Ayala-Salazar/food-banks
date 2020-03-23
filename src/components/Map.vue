c2a344048b885eb35fdfb79e887b07e8e76382cd
jaayala <jaayala@uci.edu>
2020-03-22 17:59:59 -0700

    map zooms on location clicks

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -21,6 +21,8 @@
           bounds: [[-128.8, 23.6], [-65.4, 50.2]],
           style: 'mapbox://styles/mapbox/dark-v10'
         });
+        // adds geojson sources to map
+        this.map.on('load', this.addLayer);
         /* initializes Geocoder Search box*/
         this.map.addControl(
           new MapboxGeocoder({
@@ -28,25 +30,47 @@
             countries: 'us',
             mapboxgl: mapboxgl
           }));
-        /* adds geojson sources to map */
-        this.map.on('load', this.addLayer);
+        this.map.on('click', 'main-layer',  (e) => this.addPopup(e));
+        this.map.on('click', 'main-layer',  (e) => this.map.flyTo({
+          center: e.features[0].geometry.coordinates,
+          zoom: 12
+        }));
+        this.map.on('mouseenter', 'main-layer', () => {this.map.getCanvas().style.cursor = 'pointer';});
+        this.map.on('mouseleave', 'main-layer', () => {this.map.getCanvas().style.cursor = '';});
+
+
       },
       addLayer: function () {
         this.map.addLayer({
           id: 'main-layer',
+          type: 'circle',
+          source: {
+            data: require('../assets/sample'),
+            type: 'geojson',
+          },
           paint: {
             'circle-opacity': 0.5,
             'circle-radius': 7,
             'circle-stroke-color': '#fff',
             'circle-stroke-width': 1,
             'circle-color': '#f683fc'
-          },
-          source: {
-            data: require('../assets/sample'),
-            type: 'geojson',
-          },
-          type: 'circle',
+          }
         });
+      },
+      addPopup: function (e) {
+        let coordinates = e.features[0].geometry.coordinates.slice();
+        let name = e.features[0].properties.name;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setText(name)
+          .addTo(this.map);
       }
     }
   }
