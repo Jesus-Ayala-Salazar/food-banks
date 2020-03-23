1ccb1a6ba76656a9aebbf49392671a258c684544
jaayala <jaayala@uci.edu>
2020-03-23 13:29:22 -0700

    map now resizes on button events

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <Header/>
-    <Map/>
-    <Results/>
+    <Map />
+    <Results v-b-modal/>
     <SearchBox/>
   </div>
 </template>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,6 +1,10 @@
 <template>
-    <div id="map" style="width: 100%; height: 600px;"></div>
+        <div fluid="sm" id="map" v-bind:style="styleObj"></div>
 </template>
+
+<style>
+
+</style>
 
 <script>
   import mapboxgl from 'mapbox-gl';
@@ -8,28 +12,53 @@
 
   export default {
     name: 'Map',
-    data() {return {}},
+    data() {
+      return {
+        styleObj: {
+          width: '100%',
+          height: '600px'
+        }
+      }
+    },
     mounted() {
       this.createMap();
     },
     methods: {
       /* initializes Map bounded to US */
       createMap: function () {
+        // default control for showing full width of map
+        class DefaultControl {
+          constructor(handleFunc) {
+            this.eventListener = handleFunc;
+          }
+          onAdd(map) {
+            this._map = map;
+            this._container = document.createElement('button');
+            this._container.className = 'mapboxgl-ctrl';
+            this._container.textContent = 'Default';
+            this._container.addEventListener('click', this.eventListener);
+            return this._container;
+          }
+          onRemove() {
+            this._container.parentNode.removeChild(this._container);
+            this._map = undefined;
+          }
+        }
         mapboxgl.accessToken = 'pk.eyJ1IjoiamFheWFsYSIsImEiOiJjazgxMnIxbTgwNmtsM2xwNG85MXNybTZvIn0.jgVIhoWGvlCIU5TQoEgnvA';
         this.map = new mapboxgl.Map({
           container: 'map',
           bounds: [[-128.8, 23.6], [-65.4, 50.2]],
           style: 'mapbox://styles/mapbox/dark-v10'
         });
+        let nav = new mapboxgl.NavigationControl({showCompass: false});
+        this.map.addControl(nav, 'top-left');
+
+        this.map.addControl(new DefaultControl(this.defaultView), 'top-left');
         this.addGeocoder();
         // adds geojson sources to map
         this.map.on('load', this.addLayer);
         // event listeners for showing popups when clicking main layer
-        this.map.on('click', 'main-layer',  (e) => this.addPopup(e));
-        this.map.on('click', 'main-layer',  (e) => this.map.flyTo({
-          center: e.features[0].geometry.coordinates,
-          zoom: 12
-        }));
+        this.map.on('click', 'main-layer', (e) => this.handleClick(e));
         this.map.on('mouseenter', 'main-layer', () => {this.map.getCanvas().style.cursor = 'pointer';});
         this.map.on('mouseleave', 'main-layer', () => {this.map.getCanvas().style.cursor = '';});
       },
@@ -43,10 +72,10 @@
           },
           paint: {
             'circle-opacity': 0.5,
-            'circle-radius': 7,
+            'circle-radius': 5,
             'circle-stroke-color': '#fff',
             'circle-stroke-width': 1,
-            'circle-color': '#f683fc'
+            'circle-color': '#7bfc37'
           }
         });
       },
@@ -74,8 +103,28 @@
           marker: false,
         });
         // add result event listener
-        geocoder.on('result', (result) => this.map.flyTo({center: result.result.center, zoom: 10}));
-        this.map.addControl(geocoder);
+        geocoder.on('result', (result) => this.handleResult(result));
+        this.map.addControl(geocoder);      // add geocoder to map
+      },
+      resizeContainer: function () {
+        this.styleObj.width = '50%';
+        this.map.getContainer().style.width = '50%';
+        this.map.resize();
+      },
+      handleClick: function (e) {
+        this.resizeContainer();
+        this.addPopup(e);
+        this.map.flyTo({center: e.features[0].geometry.coordinates, zoom: 10});
+      },
+      handleResult: function (res) {
+        this.resizeContainer();
+        this.map.flyTo({center: res.result.center, zoom: 10});
+      },
+      defaultView: function () {
+        this.styleObj.width = '100%';
+        this.map.getContainer().style.width = '100%';
+        this.map.resize();
+        this.map.fitBounds([[-128.8, 23.6], [-65.4, 50.2]]);
       }
     }
   }
